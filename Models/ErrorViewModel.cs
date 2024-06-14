7f58f3827b4b67b6ea8666a788e9f668243769eb
ajengfrr <ajengfebrianar@gmail.com>
2024-06-14 17:22:46 +0700

    Fix permissions and ignore .vs directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TaxForm.Models
-{
-    internal class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
